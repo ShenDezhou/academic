aa1847c2217f8f3b90eded80c05b94eaf5daa8d6
ShenDezhou <bangtech@sina.com>
2021-05-08 22:38:38 +0800

    1. Add a scroll top button.

---
+++ src/components/BackTop.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pages-content-scroll">
+    <el-backtop target=".pages-content-scroll"
+                :visibility-height="visibleHeight"
+                :right="rightPos"
+                :bottom="bottomPos"></el-backtop>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BackTop',
+  data () {
+    return {
+      visibleHeight: 200,
+      rightPos: 100,
+      bottomPos: 100
+    }
+  }
+}
+</script>
+<style scoped>
+.pages-content-scroll {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow-x: hidden;
+}
+</style>
